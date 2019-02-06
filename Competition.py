7fe34acdf47b035ec3033a3e0e68acd4259207a4
Dominik <dominobullo@gmail.com>
2019-02-06 15:15:28 +0100

    Create Competition structure, with subclass Result

--- Competition.py
+++ Competition.py
@@ -1,7 +1,20 @@
 class Competition:
-    def __init__(self, category):
-        self.category = category
+    def __init__(self, link, place):
+        self.competition_link = link
+        self.place = place
+        self.results_list = []
+        self.date = None
 
+    def set_date(self, date):
+        self.date = date
 
-t = Competition(category="žiaci")
-print(t.category)
+    class Result:
+        def __init__(self, link, date, category, gender, discipline):
+            # TODO: if date is None -> check if every date is equal
+            Competition.set_date(Competition, date)
+
+            self.date = date
+            self.result_link = link
+            self.category = category
+            self.gender = gender
+            self.discipline = discipline
