1714a2e474ee2d582e21f483129729f73752c86a
Dominik <dominobullo@gmail.com>
2019-02-10 21:26:18 +0100

    adding attributes to Racer

--- Racer.py
+++ Racer.py
@@ -1,13 +1,22 @@
 class Racer:
-    def __init__(self, name, surname, date_of_birth, gender, category, country, start_number):
-        self.name = name
-        self.surname = surname
-        self.date_of_birth = date_of_birth
+    def __init__(self, full_name, year_of_birth, gender, category, country, code, start_number, position, times,
+                 points):
+        self.surname, self.name = full_name.split(" ", 1)
+
+        self.year_of_birth = year_of_birth
+        self.code = code
         self.gender = gender
         self.category = category
         self.country = country
         self.start_number = start_number
+        self.position = position
+        self.times = times
+        self.points_earned = points
 
-        self.number_of_points = 0
+        # self.total_number_of_points += points
+
+    @property
+    def full_name(self):
+        return self.name + " " + self.surname
 
 # test = Racer("Domminik", "Bullo", "09.12.1996", "m", "kategória", "SVK")
