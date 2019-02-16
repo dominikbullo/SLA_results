dea06707d954a3e4a75307e9a1dc3f785765bf5c
Dominik <dominobullo@gmail.com>
2019-02-16 16:33:26 +0100

    update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -1,3 +1,5 @@
 # TODO: blank char
 racers = (["Bullo Dominik", '1996', 'muži'],
           ["Bullo Dominik", '2006', 'muži'])
+# you can get all racers from this site
+# http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$18.html
