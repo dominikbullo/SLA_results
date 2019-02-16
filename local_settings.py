faf4880c93faa79476b486cf6532b68b53804a2c
Dominik <dominobullo@gmail.com>
2019-02-16 17:45:53 +0100

    deleted local_settings.py file & create txt file with racers_to_find

--- local_settings.py
+++
@@ -1,5 +0,0 @@
-# TODO: blank char
-racers = (["Bullo Dominik", '1996', 'muži'],
-          ["Bullo Dominik", '2006', 'muži'])
-# you can get all racers from this site
-# http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$18.html
