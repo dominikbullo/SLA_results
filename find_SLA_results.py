f84f5c613e6947b5760fb4d05c1dfe8fa1e5e7ca
Dominik <dominobullo@gmail.com>
2019-02-17 09:01:44 +0100

    remake how to call function find results - for every club or for just list , or combination

--- find_SLA_results.py
+++ find_SLA_results.py
@@ -99,14 +99,16 @@ def find_competitions_links(start_number, number_of_rounds):
 
 
 def find_club_name_in_database(ski_club_name):
-    # TODO: database of clubs into text file, to check if club is good or wrong spelled
+    # TODO: database of clubs_list into text file, to check if club is good or wrong spelled
     return True
 
 
 def find_racers_by_club(ski_club):
     # "žiaci":"http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$18.html"
     # "predžiaci": "http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$17.html"
+    # "zapocitane", http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/zapoctene$17.html
     # TODO: find racers_with_club_and_points_list by given urls
+    racers_list = []
     racers_with_club_and_points_list = [
         "http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$17:MP:M.html",
         "http://www.slovak-ski.sk/zjazdove-lyzovanie/pohare/jednotlivci$17:MP:L.html",
@@ -122,11 +124,27 @@ def find_racers_by_club(ski_club):
                 test = row.findChildren('td')
                 for cell in test[4]:
                     if str(cell.string).capitalize() == ski_club.capitalize():
-                        racers.append([test[2].text, test[3].text, gender])
+                        racers_list.append([test[2].text, test[3].text, gender])
             except IndexError:
                 pass
                 continue
-    print('Zoznam nájdených pretekárov:', *racers, sep='\n- ')
+    print('Zoznam nájdených pretekárov:', *racers_list, sep='\n- ')
+    return racers_list
+
+
+def find_results(racers_list):
+    categories = {
+        "Predžiaci": find_competitions_links(651, 8),
+        # "Žiaci": find_competitions_links(645, 6)
+    }
+
+    for competition_links in categories.values():
+        print('Zoznam podujatí:', *competition_links, sep='\n- ')
+        finder = ResultsFinder(competition_links, create_racers_list(racers_list))
+        finder.create_competitions_list_with_results()
+        finder.print_results_list()
+
+        # finder.write_results_into_excel()
 
 
 if __name__ == "__main__":
@@ -135,23 +153,24 @@ if __name__ == "__main__":
     # TODO: WARNING !!!!!!!!!!!!!!! by club - just racers with points!!!!!!!!!!!!!!!!!!!!!
     # TODO: find and save link a href  and read from this link
     # TODO: With poinst there ist list of race right on this racer <a href link>
+    # TODO: create some database about racer and his page, club and so on for better speed
 
     parser = argparse.ArgumentParser(description='App for searching results from slovak-ski.sk')
     parser.add_argument('-sc', '--ski_club_name', type=str,
                         help='Argument for full ski club name, for searching result for racers from this club',
                         default=None)
+    parser.add_argument('-cb', '--combine_search', action='store_true', default=True,
+                        help='If you want to find racers by club, and add some racers from file use this')
+
     parser.add_argument('-rl', '--by_racers_list', action='store_true', default=False,
-                        help='If you want to specify list of racers, e.g. from multiple clubs')
+                        help='If you want to specify list of racers, e.g. from multiple clubs_list')
 
     args = parser.parse_args()
 
-    categories = {
-        "Predžiaci": find_competitions_links(651, 8),
-        #"Žiaci": find_competitions_links(645, 6)
-    }
-
     racers = []
-    if args.by_racers_list:
+
+    # TODO: Remake to work
+    if args.by_racers_list or args.combine_search:
         try:
             with open('racers_to_find_list.txt', 'r') as f:
                 for line in f:
@@ -159,21 +178,18 @@ if __name__ == "__main__":
                     racers.append(inner_list)
         except IOError:
             print('fCannot read {f.name}')
-    else:
-        clubs = args.ski_club_name.split(",")
-        for club in clubs:
-            if args.ski_club_name is None and find_club_name_in_database(clubs):
-                ski_club_name = input("Insert the proper name of ski club for which you want to find results")
-            else:
-                ski_club_name = club
+
+    if not args.by_racers_list and args.ski_club_name is not None:
+        clubs_list = args.ski_club_name.split(",")
+
+        for club in clubs_list:
+            if not find_club_name_in_database(ski_club_name=club):
+                club = input("Insert the proper name of ski club for which you want to find results")
+
+            # correct append racers in list
+            [racers.append(x) for x in find_racers_by_club(club)]
             print(f'Start finding results for club {club}')
 
-            find_racers_by_club(club)
-
-            for competition_links in categories.values():
-                print('Zoznam podujatí:', *competition_links, sep='\n- ')
-                finder = ResultsFinder(competition_links, create_racers_list(racers))
-                finder.create_competitions_list_with_results()
-                finder.print_results_list()
-
-            # finder.write_results_into_excel()
+            find_results(racers)
+    else:
+        find_results(racers)
